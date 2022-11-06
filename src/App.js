6142800bf127d1e878aac622a15e19b13c03ea72
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-11-06 11:16:56 +0500

    add img frontend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="Page">
 
       <div className='Header'>
-        шапка
+        <img src='https://sun9-west.userapi.com/sun9-1/s/v1/ig2/D-elL-GHz9_lpeq9AwLr_WPYYEA4NnWqwpn49TQL4_J_bu03zFruNkDjqY6LVRbPBMTrjuN8k9OM3c7BynN1e1GN.jpg?size=1202x193&quality=96&type=album' alt='frontEnd' />
       </div>
 
       <div className='NavBar'>
