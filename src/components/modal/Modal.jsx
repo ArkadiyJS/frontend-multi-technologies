3ca998a38088585730abc8d1835d8219c4975f5a
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-11-08 21:42:14 +0500

    minor fixed css paddings

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -8,6 +8,7 @@ const Modal = (props) => {
   return (
     <>
       <button className={s.openModalBtn} onClick={() => setShowModal(true)}>✨ Открыть окно</button>
+
       {showModal ? <div className={s.overlay}>
         <div className={s.modal}>
           <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setShowModal(false)}>
@@ -17,7 +18,18 @@ const Modal = (props) => {
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif' />
         </div>
       </div> : ''}
+
       <button className={s.openModalBtn} onClick={() => alert('hi 2 window')}>✨ Открыть окно 2</button>
+
+      {/* <div className={s.overlay}>
+        <div className={s.modal}>
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setShowModal(false)}>
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif' />
+        </div>
+      </div> */}
 
     </>
   );
