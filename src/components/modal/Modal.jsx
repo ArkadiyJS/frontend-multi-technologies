e0a545cf179bd1eab60abf48c9590371478ca468
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-11-08 20:34:53 +0500

    add logic in  Modal component 1 step of 2

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import s from './Modal.module.css';
 
 const Modal = (props) => {
+
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <>
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+      <button className={s.openModalBtn} onClick={() => setShowModal(true)}>✨ Открыть окно</button>
+      {showModal ? <div className={s.overlay}>
+        <div className={s.modal}>
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setShowModal(false)}>
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif' />
         </div>
-      </div>
+      </div> : ''}
+      <button className={s.openModalBtn} onClick={() => alert('hi 2 window')}>✨ Открыть окно 2</button>
+
     </>
   );
 }
