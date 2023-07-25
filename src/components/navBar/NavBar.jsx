43f8f3ba2ea31f9437a6e4602fa680e3e3573ef7
Arkadiy Vologodskiy <vologodskiy20@gmail.com>
2023-07-25 10:33:51 +0500

    del hr

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -7,24 +7,24 @@ function NavBar(props) {
     <div >
       <div className={s.text}>elements frontEnds</div>
 
-      <hr />
+     
 
 
       <NavLink to='modal'><button className={s.button} >Modal</button></NavLink>
 
 
-      <hr />
+      
 
 
       <NavLink to='quiz'><button className={s.button} >Quiz </button></NavLink>
 
 
-      <hr />
+      
 
 
       <NavLink to='gallery'><button className={s.button} >Photo gallery</button></NavLink>
 
-      <hr />
+      
 
     </div>
   );
