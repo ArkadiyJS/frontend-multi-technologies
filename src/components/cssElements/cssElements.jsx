9b58a311ea6986e772bb483847bbd5363c769d39
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-12-25 16:23:18 +0500

    rendering by condition

--- src/components/cssElements/cssElements.jsx
+++ src/components/cssElements/cssElements.jsx
@@ -1,11 +1,46 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import BorderAnimation from './elements/borderAnimation';
 
 function CSSElements() {
+
+
+
+
+
+  const pickCssElements = ['Бордер', 'Вентелятор',]
+
+
+  const [showCssElements, setShowCssElements] = useState(false)
+
+  const [pickElement, setPickElement] = useState('')
+
+
+
+
+
+
+
   return (
     <div>
-      <BorderAnimation />
+      <ul onClick={() => { setShowCssElements(!showCssElements) }}><h1>Выбери</h1>
+
+        {showCssElements ? pickCssElements.map((CssElements, i) => (
+          <li>
+
+            <button onClick={() => setPickElement(CssElements)} className='button' key={i}>
+              {CssElements}
+            </button>
+
+          </li>)) : ''}
+
+      </ul>
+
+      <h1>{pickElement}</h1>
+
+      {/* рендер выбраного елемента в списке */}
+
+      {pickElement == 'Бордер' && <BorderAnimation />}
 
 
 
