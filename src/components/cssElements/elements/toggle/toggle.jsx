a3b5e691cdbee3421d4e608759bb0d92b6f7eedc
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-01-23 20:16:30 +0500

    checbox

---
+++ src/components/cssElements/elements/toggle/toggle.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Toggle(props) {
+  return (
+    <>
+      <label>
+        <input type='checbox' />
+
+
+      </label>
+
+    </>
+  );
+}
+
+export default Toggle;
