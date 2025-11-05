daa12dfaec919e3de1ba5df4b4145ee398b65bbd
ArkadiyJS <vologodskiy20@gmail.com>
2025-11-05 12:46:02 +0500

    bad chages

---
+++ frontend-multi-technologies/src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React>
+// );
+
+createRoot(document.getElementById("root")).render(
+  <React>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React>
+);
